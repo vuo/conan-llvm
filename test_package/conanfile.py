63dbc9cc37d637fb6aaeee34943be4325e54b22e
Steve Mokris <smokris@softpixel.com>
2018-04-05 19:40:00 -0400

    Ensure we don't link to libstdc++.

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -29,9 +29,11 @@ class LlvmTestConan(ConanFile):
             self.output.info('Checking %s...' % f)
             if platform.system() == 'Darwin':
                 self.run('! (otool -L ' + f + ' | tail +2 | egrep -v "^\s*(/usr/lib/|/System/|@rpath/)")')
+                self.run('! (otool -L ' + f + ' | fgrep "libstdc++")')
                 self.run('! (otool -l ' + f + ' | grep -A2 LC_RPATH | cut -d"(" -f1 | grep "\s*path" | egrep -v "^\s*path @(executable|loader)_path/")')
             elif platform.system() == 'Linux':
                 self.run('! (ldd ' + f + ' | grep -v "^lib/" | grep "/" | egrep -v "(\s(/lib64/|(/usr)?/lib/x86_64-linux-gnu/)|test_package/build)")')
+                self.run('! (ldd ' + f + ' | fgrep "libstdc++")')
 
         libs = self.deps_cpp_info['llvm'].libs
         libs.remove('c++abi')
@@ -39,6 +41,8 @@ class LlvmTestConan(ConanFile):
             self.output.info('Checking %s...' % f)
             if platform.system() == 'Darwin':
                 self.run('! (otool -L lib/lib' + f + '.dylib | tail +2 | egrep -v "^\s*(/usr/lib/|/System/|@rpath/)")')
+                self.run('! (otool -L lib/lib' + f + '.dylib | fgrep "libstdc++")')
                 self.run('! (otool -l lib/lib' + f + '.dylib | grep -A2 LC_RPATH | cut -d"(" -f1 | grep "\s*path" | egrep -v "^\s*path @(executable|loader)_path")')
             elif platform.system() == 'Linux':
                 self.run('! (ldd lib/lib' + f + '.so | grep -v "^lib/" | grep "/" | egrep -v "(\s(/lib64/|(/usr)?/lib/x86_64-linux-gnu/)|test_package/build)")')
+                self.run('! (ldd lib/lib' + f + '.so | fgrep "libstdc++")')
